a4189d786bdcdb6463308670796cdf46c3d12039
katakk <katakk@cogentoo>
2014-02-11 03:38:35 -0800

    modified:   config.h

--- config.h
+++ config.h
@@ -1,67 +1,20 @@
-#define HAVE_ARPA_INET_H 1
-#define HAVE_ATEXIT 1
-#define HAVE_DECL_AF_INET6 1
-#define HAVE_DECL_IPV6_ADD_MEMBERSHIP 1
-#define HAVE_DECL_IPV6_MULTICAST_HOPS 1
-#define HAVE_DECL_IP_ADD_MEMBERSHIP 1
-#define HAVE_FORK 1
-#define HAVE_GETTIMEOFDAY 1
-#define HAVE_INET_NTOP 1
-#define HAVE_INET_PTON 1
-#define HAVE_INTTYPES_H 1
-#define HAVE_IPV6 1
-#define HAVE_IPV6_MULTICAST 1
-#define HAVE_LIBINTL_H 1
-#define HAVE_MALLOC 1
-#define HAVE_MEMORY_H 1
-#define HAVE_MEMSET 1
-#define HAVE_MULTICAST 1
-#define HAVE_NETDB_H 1
-#define HAVE_NETINET_IN_H 1
-#define HAVE_POSIX_THREAD 1
-#define HAVE_PTHREAD 1
-#define HAVE_QUAD_SUPPORT 1
-#define HAVE_SELECT 1
-#define HAVE_SNPRINTF 1
-#define HAVE_SSIZE_T 1
-#define HAVE_STDINT_H 1
-#define HAVE_STDLIB_H 1
-#define HAVE_STRCHR 1
-#define HAVE_STRERROR 1
-#define HAVE_STRFTIME 1
-#define HAVE_STRINGS_H 1
-#define HAVE_STRING_H 1
-#define HAVE_STRTOL 1
-#define HAVE_STRUCT_IPV6_MREQ 1
-#define HAVE_STRUCT_IP_MREQ 1
-#define HAVE_STRUCT_SOCKADDR_IN6 1
-#define HAVE_STRUCT_SOCKADDR_STORAGE 1
-#define HAVE_SYSLOG_H 1
-#define HAVE_SYS_SELECT_H 1
-#define HAVE_SYS_SOCKET_H 1
-#define HAVE_SYS_STAT_H 1
-#define HAVE_SYS_TIME_H 1
-#define HAVE_SYS_TYPES_H 1
-#define HAVE_UNISTD_H 1
-#define HAVE_USLEEP 1
-#define HAVE_VFORK 1
-#define HAVE_VPRINTF 1
-#define HAVE_WORKING_FORK 1
-#define HAVE_WORKING_VFORK 1
-#define PACKAGE "iperf"
-#define PACKAGE_BUGREPORT ""
-#define PACKAGE_NAME "Iperf"
-#define PACKAGE_STRING "Iperf 2.0.5"
-#define PACKAGE_TARNAME "iperf"
-#define PACKAGE_VERSION "2.0.5"
-#define RETSIGTYPE void
-#define SELECT_TYPE_ARG1 int
-#define SELECT_TYPE_ARG234 (fd_set *)
-#define SELECT_TYPE_ARG5 (struct timeval *)
-#define SIZEOF_BOOL 0
-#define STDC_HEADERS 1
-#define Socklen_t socklen_t
-#define TIME_WITH_SYS_TIME 1
-#define VERSION "2.0.5"
-#define _REENTRANT 1
-#define bool int
+
+#define HAVE_GETTIMEOFDAY       1 
+#define HAVE_INET_NTOP          1 
+#define HAVE_INET_PTON          1 
+#undef  HAVE_INT32_T              
+#define HAVE_IPV6 1               
+#define HAVE_IPV6_MULTICAST     1 
+#define HAVE_LIBINTL_H          1 
+#define HAVE_MULTICAST          1 
+#define HAVE_POSIX_THREAD       1 
+#undef  HAVE_PRINTF_QD            
+#define HAVE_PTHREAD            1 
+#undef  HAVE_PTHREAD_CANCEL       
+#define HAVE_QUAD_SUPPORT       1 
+#define HAVE_SNPRINTF           1 
+#define HAVE_STRINGS_H          1 
+#undef  HAVE_THREAD               
+#define Socklen_t socklen_t       
+#define _REENTRANT              1 
+#define bool int                  
