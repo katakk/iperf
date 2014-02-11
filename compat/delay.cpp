2c13207c65d2cb4287db0b674b236b193c7a98b6
kata-chan <kata@katakuri.sakura.ne.jp>
2014-02-12 01:17:57 +0900

    15    A new compat/?delay.cpp     None    open        2010-07-17  2010-07-17  5
    	modified:   compat/delay.cpp

--- compat/delay.cpp
+++ compat/delay.cpp
@@ -1,76 +1,111 @@
-/*--------------------------------------------------------------- 
- * Copyright (c) 1999,2000,2001,2002,2003                              
- * The Board of Trustees of the University of Illinois            
- * All Rights Reserved.                                           
- *--------------------------------------------------------------- 
- * Permission is hereby granted, free of charge, to any person    
- * obtaining a copy of this software (Iperf) and associated       
- * documentation files (the "Software"), to deal in the Software  
- * without restriction, including without limitation the          
- * rights to use, copy, modify, merge, publish, distribute,        
- * sublicense, and/or sell copies of the Software, and to permit     
+/*---------------------------------------------------------------
+ * Copyright (c) 1999,2000,2001,2002,2003,2010
+ * The Board of Trustees of the University of Illinois
+ * All Rights Reserved.
+ *---------------------------------------------------------------
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software (Iperf) and associated
+ * documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit
  * persons to whom the Software is furnished to do
- * so, subject to the following conditions: 
+ * so, subject to the following conditions:
  *
- *     
- * Redistributions of source code must retain the above 
- * copyright notice, this list of conditions and 
- * the following disclaimers. 
  *
- *     
- * Redistributions in binary form must reproduce the above 
- * copyright notice, this list of conditions and the following 
- * disclaimers in the documentation and/or other materials 
- * provided with the distribution. 
- * 
- *     
- * Neither the names of the University of Illinois, NCSA, 
- * nor the names of its contributors may be used to endorse 
+ * Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and
+ * the following disclaimers.
+ *
+ *
+ * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimers in the documentation and/or other materials
+ * provided with the distribution.
+ *
+ *
+ * Neither the names of the University of Illinois, NCSA,
+ * nor the names of its contributors may be used to endorse
  * or promote products derived from this Software without
- * specific prior written permission. 
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
- * NONINFRINGEMENT. IN NO EVENT SHALL THE CONTIBUTORS OR COPYRIGHT 
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ * specific prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE CONTIBUTORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * ________________________________________________________________
- * National Laboratory for Applied Network Research 
- * National Center for Supercomputing Applications 
- * University of Illinois at Urbana-Champaign 
+ * National Laboratory for Applied Network Research
+ * National Center for Supercomputing Applications
+ * University of Illinois at Urbana-Champaign
  * http://www.ncsa.uiuc.edu
- * ________________________________________________________________ 
+ * ________________________________________________________________
  *
  * delay.c
- * by Mark Gates <mgates@nlanr.net>
+ * by Kirby Zhou <kirbyzhou at gmail.com>
  * -------------------------------------------------------------------
  * accurate microsecond delay
  * ------------------------------------------------------------------- */
 
 #include "Timestamp.hpp"
-#include "util.h"
+
 #include "delay.hpp"
 
+
 /* -------------------------------------------------------------------
- * A micro-second delay function using POSIX nanosleep(). This allows a
- * higher timing resolution (under Linux e.g. it uses hrtimers), does not
- * affect any signals, and will use up remaining time when interrupted.
+ * determine the timer resolution of nanosleep
  * ------------------------------------------------------------------- */
-void delay_loop(unsigned long usec)
+static long measure_nanosleep_threshold()
 {
-    struct timespec requested, remaining;
+	timespec req = {0, 1000};
+	Timestamp bg, ed;
+	bg.setnow();
+	for (int i = 0; i < 20; ++i) {
+		nanosleep(&req, NULL);
+	}
+	ed.setnow();
+	long diff = ed.subUsec(bg) / 20;
+	if (diff <= 20) {
+		diff = 10;
+	} else if (diff <= 200 ) {
+		diff = 10 * ( (diff+5) / 10 ) - 5;
+	} else if (diff <= 600 ) {
+		diff = 50 * ( (diff+25) / 50 ) - 50;
+	} else if (diff <= 2000 ) {
+		diff = 100 * ( (diff+50) / 100 ) - 50;
+	} else {
+		diff = 500 * ( (diff+250) / 500 ) - 50;
+	}
+	printf("thr %ld\n", diff);
+	return diff;
+}
+static long nanosleep_threshold = measure_nanosleep_threshold();
 
-    requested.tv_sec  = 0;
-    requested.tv_nsec = usec * 1000L;
+/* -------------------------------------------------------------------
+ * A micro-second delay function. This uses gettimeofday (underneith
+ * the Timestamp) which has a resolution of upto microseconds. I've
+ * found it's good to within about 10 usecs.
+ * I used to do calibration, but iperf automatically adjusts itself
+ * so that isn't necesary, and it causes some problems if the
+ * calibration adjustment is larger than your sleep time.
+ * ------------------------------------------------------------------- */
 
-    while (nanosleep(&requested, &remaining) == -1)
-        if (errno == EINTR)
-            requested = remaining;
-        else {
-            WARN_errno(1, "nanosleep");
-            break;
+void delay_loop( unsigned long usec ) {
+    Timestamp end;
+    end.add( usec * 1e-6 );
+
+    Timestamp now;
+    long diff;
+    while ( (diff = end.subUsec(now)) > 0 ) {
+        if (diff >= nanosleep_threshold) {
+            timespec req = { 0, usec * 1000 };
+            nanosleep(&req, NULL);
+        } else {
+            sched_yield();
         }
+        now.setnow();
+    }
 }
