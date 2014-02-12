86a379356d5ac39e26c4099008c5dbf65b58face
katakk <katakk@linuxBean>
2014-02-12 22:18:53 +0900

    vc2005 not have stdint.h 	modified:   include/iperf-int.h

--- include/iperf-int.h
+++ include/iperf-int.h
@@ -2,8 +2,9 @@
 #define _IPERF_INCLUDE_IPERF_INT_H 1
 #ifndef _GENERATED_STDINT_H
 #define _GENERATED_STDINT_H "iperf 2.0.5"
-/* generated using gnu compiler gcc (GCC) 4.1.2 (Gentoo 4.1.2) */
+#ifndef WIN32
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
+#endif
 #endif
 #endif
