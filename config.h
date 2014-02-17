71aa0d3ed31caacaf67cc8335f757d74c11dc6cd
test <test@cogentoo>
2014-02-17 22:26:56 +0900

    BUG delete mm timer

--- config.h
+++ config.h
@@ -11,8 +11,6 @@
 #    include <ws2tcpip.h>
 #    include <windows.h>
 #pragma comment(lib, "ws2_32.lib")
-#    include <mmsystem.h>
-#pragma comment(lib, "winmm.lib")
 
 #    define HAVE_WIN32_THREAD 1
 #else /* WIN32 */
