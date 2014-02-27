c0132f51e0e567b1ed7654d6f7f3096bb5656634
kata <kata@katakuri.sakura.ne.jp>
2014-02-27 23:50:17 +0900

    https://github.com/katakk/iperf/issues/30

--- gui/IperfThread.cpp
+++ gui/IperfThread.cpp
@@ -13,7 +13,7 @@ IMPLEMENT_DYNCREATE(CIperfThread, CWinThread)
 
 CIperfThread::CIperfThread()
 {
-	m_bAutoDelete = TRUE;
+    m_bAutoDelete = TRUE;
 }
 
 CIperfThread::~CIperfThread()
@@ -29,174 +29,193 @@ END_MESSAGE_MAP()
 #define TotalBytes 4096
 void CIperfThread::CreateIperfPipe()
 {
-	HANDLE hReadPipeOut;
-	HANDLE hReadPipeErr;
-	HANDLE hReadPipeInp;
-	HANDLE hWritePipeOut;
-	HANDLE hWritePipeErr;
-	HANDLE hWritePipeInp;
+    HANDLE hReadPipeOut;
+    HANDLE hReadPipeErr;
+    HANDLE hReadPipeInp;
+    HANDLE hWritePipeOut;
+    HANDLE hWritePipeErr;
+    HANDLE hWritePipeInp;
 
-	HANDLE hParent = GetCurrentProcess();
+    HANDLE hParent = GetCurrentProcess();
 
-	SECURITY_ATTRIBUTES security;
-	security.nLength = sizeof(SECURITY_ATTRIBUTES);
-	security.lpSecurityDescriptor = NULL;
-	security.bInheritHandle = TRUE;
+    SECURITY_ATTRIBUTES security;
+    security.nLength = sizeof(SECURITY_ATTRIBUTES);
+    security.lpSecurityDescriptor = NULL;
+    security.bInheritHandle = TRUE;
 
-	if(!CreatePipe(&hReadPipeOut,&hWritePipeOut,&security,0)) ASSERT(0);// ("CreatePipe");
-	if(!CreatePipe(&hReadPipeErr,&hWritePipeErr,&security,0)) ASSERT(0);// ("CreatePipe");
-	if(!CreatePipe(&hReadPipeInp,&hWritePipeInp,&security,0)) ASSERT(0);// ("CreatePipe");
-	
-	SetStdHandle(STD_OUTPUT_HANDLE,hWritePipeOut);
-	SetStdHandle(STD_ERROR_HANDLE, hWritePipeErr);
-	SetStdHandle(STD_INPUT_HANDLE, hReadPipeInp);
-	
-	DuplicateHandle(hParent,hReadPipeOut,hParent,&m_hPipeOut,0,FALSE,DUPLICATE_SAME_ACCESS);
-	DuplicateHandle(hParent,hReadPipeErr,hParent,&m_hPipeErr,0,FALSE,DUPLICATE_SAME_ACCESS);
-	DuplicateHandle(hParent,hWritePipeInp,hParent,&m_hPipeIn,0,FALSE,DUPLICATE_SAME_ACCESS);
-	
-	CloseHandle(hReadPipeOut);
-	CloseHandle(hReadPipeErr);
-	CloseHandle(hWritePipeInp);
+    if(!CreatePipe(&hReadPipeOut,&hWritePipeOut,&security,0)) ASSERT(0);// ("CreatePipe");
+    if(!CreatePipe(&hReadPipeErr,&hWritePipeErr,&security,0)) ASSERT(0);// ("CreatePipe");
+    if(!CreatePipe(&hReadPipeInp,&hWritePipeInp,&security,0)) ASSERT(0);// ("CreatePipe");
+    
+    SetStdHandle(STD_OUTPUT_HANDLE,hWritePipeOut);
+    SetStdHandle(STD_ERROR_HANDLE, hWritePipeErr);
+    SetStdHandle(STD_INPUT_HANDLE, hReadPipeInp);
+    
+    DuplicateHandle(hParent,hReadPipeOut,hParent,&m_hPipeOut,0,FALSE,DUPLICATE_SAME_ACCESS);
+    DuplicateHandle(hParent,hReadPipeErr,hParent,&m_hPipeErr,0,FALSE,DUPLICATE_SAME_ACCESS);
+    DuplicateHandle(hParent,hWritePipeInp,hParent,&m_hPipeIn,0,FALSE,DUPLICATE_SAME_ACCESS);
+    
+    CloseHandle(hReadPipeOut);
+    CloseHandle(hReadPipeErr);
+    CloseHandle(hWritePipeInp);
 }
 
 void CIperfThread::ReadIperfPipe(HANDLE hPipe)
 {
-	CHAR str[TotalBytes];
-	DWORD BytesRead;
-	DWORD TotalBytesAvail;
-	DWORD BytesLeftThisMessage;
-	DWORD dwByte;
+    CHAR str[TotalBytes];
+    DWORD BytesRead;
+    DWORD TotalBytesAvail;
+    DWORD BytesLeftThisMessage;
+    DWORD dwByte;
 
-	FlushFileBuffers(hPipe);
-	PeekNamedPipe(hPipe, str, TotalBytes, &BytesRead,
+    FlushFileBuffers(hPipe);
+    PeekNamedPipe(hPipe, str, TotalBytes, &BytesRead,
           &TotalBytesAvail, &BytesLeftThisMessage );
-	if( BytesRead > 0 )
-	{
-		ReadFile(hPipe,str,BytesRead,&dwByte,NULL);
-		str[dwByte] = '\0';
-		m_pMainWnd->SendMessage(WM_CONSOLE_MESG, m_uniqid, (LPARAM)str);
-	}
+    if( BytesRead > 0 )
+    {
+        ReadFile(hPipe,str,BytesRead,&dwByte,NULL);
+        str[dwByte] = '\0';
+        m_pMainWnd->SendMessage(WM_CONSOLE_MESG, m_uniqid, (LPARAM)str);
+    }
 
 }
 
-BOOL WindowShowOrHidden(HWND hWnd, int nCmdShow)
+BOOL CALLBACK WindowHidden(HWND hWnd, LPARAM lParam)
 {
-	TCHAR moduleName[ _MAX_PATH * 2 ];
-	TCHAR szclass[_MAX_PATH * 2];
-	TCHAR szname[_MAX_PATH * 2];
+    PROCESS_INFORMATION *ProcessInfo = (PROCESS_INFORMATION *)lParam;
+    TCHAR moduleName[ _MAX_PATH * 2 ];
+    TCHAR szclass[_MAX_PATH * 2];
+    TCHAR szname[_MAX_PATH * 2];
+    DWORD dwAppProcessId;
+    DWORD dwAppThreadId;
+    DWORD dwCmdProcessId;
+    DWORD dwCmdThreadId;
 
-	GetClassName( hWnd, szclass, _MAX_PATH );
-	if( _tcscmp( szclass, _T("ConsoleWindowClass") ) == 0 )
-	{
-		CWinApp *pApp = ::AfxGetApp();
-		::GetModuleFileName(
-			::GetModuleHandle( pApp->m_pszExeName ),
-			 moduleName, sizeof( moduleName ) ); 
-		GetWindowText( hWnd, szname, 255 );
+    dwCmdThreadId = GetWindowThreadProcessId( hWnd, &dwCmdProcessId);
+    dwAppThreadId = GetWindowThreadProcessId( AfxGetApp()->GetMainWnd()->GetSafeHwnd(), &dwAppProcessId);
+     
+    if( 
+        // 同一プロセス
+        dwAppProcessId == dwCmdProcessId && 
+        (GetWindow(hWnd,GW_OWNER) == 0) &&
+        IsWindowVisible(hWnd) &&
+        // メインウインドウじゃない
+        dwAppThreadId != dwCmdThreadId )
+    {
+        
+        GetClassName( hWnd, szclass, _MAX_PATH );
+        GetWindowText( hWnd, szname, 255 );
+        GetModuleFileName(GetModuleHandle( AfxGetApp()->m_pszExeName ),
+                  moduleName, sizeof( moduleName ) ); 
 
-		if( _tcscmp( szname, moduleName ) == 0 )
-			::ShowWindow( hWnd, nCmdShow );
-	}
-	return TRUE;
-}
+        /*
+            CString info;
+            info.Format(_T("***[%s:%s] %x %x %x %x\n"), szclass, szname,
+                dwAppProcessId,  dwCmdProcessId,
+                dwAppThreadId,   dwCmdThreadId
+                );
+            AfxGetApp()->GetMainWnd()->SendMessage(WM_CONSOLE_MESG, 0, (LPARAM)(LPCTSTR)info);
+        */
 
-BOOL CALLBACK WindowHidden(HWND hWnd, LPARAM)
-{
-	return WindowShowOrHidden(hWnd, SW_HIDE);
-}
-
-BOOL CALLBACK WindowShow(HWND hWnd, LPARAM)
-{
-	return WindowShowOrHidden(hWnd, SW_SHOWNORMAL);
+        // コンソール窓
+        if( _tcscmp( szclass, _T("ConsoleWindowClass") ) == 0)
+        {
+            ::ShowWindow( hWnd, SW_HIDE );
+        }
+    }
+    return TRUE;
 }
 
 BOOL CIperfThread::InitInstance()
 {
-	// TODO:  スレッドごとの初期化をここで実行します。
-	HANDLE hOldIn = GetStdHandle(STD_INPUT_HANDLE);
-	HANDLE hOldOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	HANDLE hOldErr = GetStdHandle(STD_ERROR_HANDLE);
+    // TODO:  スレッドごとの初期化をここで実行します。
+    HANDLE hOldIn = GetStdHandle(STD_INPUT_HANDLE);
+    HANDLE hOldOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    HANDLE hOldErr = GetStdHandle(STD_ERROR_HANDLE);
 
-	STARTUPINFOA si;
-	memset(&si,0,sizeof(STARTUPINFO));
-	si.cb = sizeof(STARTUPINFO);
+    STARTUPINFOA si;
+    memset(&si,0,sizeof(STARTUPINFO));
+    si.cb = sizeof(STARTUPINFO);
+    si.dwFlags |= STARTF_USESHOWWINDOW;
+    si.wShowWindow = SW_HIDE;
 
-	m_uniqid = mkhash(m_CmdLine, m_uniqid) ;
-	m_uniqid += (WORD)m_nThreadID;
-	m_uniqid += (WORD)m_nThreadID >> 4;
 
-	FreeConsole();
-	AllocConsole();
+    m_uniqid = mkhash(m_CmdLine, m_uniqid) ;
+    m_uniqid += (WORD)m_nThreadID;
+    m_uniqid += (WORD)m_nThreadID >> 4;
 
-	CreateIperfPipe();
+    FreeConsole();
+    AllocConsole();
 
-	if(CreateProcess(NULL, m_CmdLine, NULL, NULL, TRUE, 0,
-	    NULL, NULL, &si, &m_ProcessInfo) == FALSE ) {
-		return FALSE;
-	}
+    CreateIperfPipe();
 
-	WaitForInputIdle(m_ProcessInfo.hProcess, INFINITE);
-	SetStdHandle(STD_OUTPUT_HANDLE,hOldOut);
-	SetStdHandle(STD_INPUT_HANDLE,hOldIn);
-	SetStdHandle(STD_ERROR_HANDLE,hOldErr);
-	EnumWindows(WindowHidden, NULL);
-	return TRUE;
+    if(CreateProcess(NULL, m_CmdLine, NULL, NULL, TRUE, 0,
+        NULL, NULL, &si, &m_ProcessInfo) == FALSE ) {
+        return FALSE;
+    }
+
+    EnumWindows(WindowHidden, (LPARAM)(PROCESS_INFORMATION *)&m_ProcessInfo);
+    WaitForInputIdle(m_ProcessInfo.hProcess, INFINITE);
+    SetStdHandle(STD_OUTPUT_HANDLE,hOldOut);
+    SetStdHandle(STD_INPUT_HANDLE,hOldIn);
+    SetStdHandle(STD_ERROR_HANDLE,hOldErr);
+    EnumWindows(WindowHidden, (LPARAM)(PROCESS_INFORMATION *)&m_ProcessInfo);
+    return TRUE;
 }
 
 int CIperfThread::ExitInstance()
 {
-	// TODO:  スレッドごとの後処理をここで実行します。
-	GetExitCodeProcess(m_ProcessInfo.hProcess, &m_exitCode);
-	CloseHandle(m_ProcessInfo.hThread);
-	TerminateProcess(m_ProcessInfo.hProcess, 0);
+    // TODO:  スレッドごとの後処理をここで実行します。
+    GetExitCodeProcess(m_ProcessInfo.hProcess, &m_exitCode);
+    CloseHandle(m_ProcessInfo.hThread);
+    TerminateProcess(m_ProcessInfo.hProcess, 0);
 
-	// post exit
-	m_pMainWnd->SendMessage(WM_CONSOLE_QUIT, m_uniqid, (LPARAM)this);
+    // post exit
+    m_pMainWnd->SendMessage(WM_CONSOLE_QUIT, m_uniqid, (LPARAM)this);
 
-	CloseHandle(m_ProcessInfo.hProcess);
-	TerminateProcess(m_ProcessInfo.hProcess, 0);
-	FreeConsole();
-	CWinThread::ExitInstance();
+    CloseHandle(m_ProcessInfo.hProcess);
+    TerminateProcess(m_ProcessInfo.hProcess, 0);
+    FreeConsole();
+    CWinThread::ExitInstance();
 
-	delete this;
+    delete this;
 
-	return 0; 
+    return 0; 
 }
 
 int CIperfThread::OnIdle(LONG lCount)
 {
-	// TODO : ここに特定なコードを追加するか、もしくは基本クラスを呼び出してください。
-	if(WaitForSingleObject(m_ProcessInfo.hProcess, 0) != WAIT_OBJECT_0)
-	{
-		FlushFileBuffers(m_hPipeErr);
-		FlushFileBuffers(m_hPipeOut);
-		FlushFileBuffers(m_hPipeIn);
+    // TODO : ここに特定なコードを追加するか、もしくは基本クラスを呼び出してください。
+    if(WaitForSingleObject(m_ProcessInfo.hProcess, 0) != WAIT_OBJECT_0)
+    {
+        FlushFileBuffers(m_hPipeErr);
+        FlushFileBuffers(m_hPipeOut);
+        FlushFileBuffers(m_hPipeIn);
 
-		ReadIperfPipe(m_hPipeErr);
-		ReadIperfPipe(m_hPipeOut);
-		return 1;
-	}
-	return 0;
+        ReadIperfPipe(m_hPipeErr);
+        ReadIperfPipe(m_hPipeOut);
+        return 1;
+    }
+    return 0;
 }
 
 // やっぱオーバライドしないと動きおかしい
 int CIperfThread::Run()
 {
-	ASSERT_VALID(this);
-	_AFX_THREAD_STATE* pState = AfxGetThreadState();
+    ASSERT_VALID(this);
+    _AFX_THREAD_STATE* pState = AfxGetThreadState();
 
-	do {
-		if(! OnIdle(1)) 
-		{
-			if(WaitForSingleObject(m_ProcessInfo.hProcess,
-				INFINITE) == WAIT_OBJECT_0)
-			{
-				break;
-			}
-		}
-	}	while(1);	
-	
-	ExitInstance();
-	return 0;
+    do {
+        if(! OnIdle(1)) 
+        {
+            if(WaitForSingleObject(m_ProcessInfo.hProcess,
+                INFINITE) == WAIT_OBJECT_0)
+            {
+                break;
+            }
+        }
+    }   while(1);   
+    
+    ExitInstance();
+    return 0;
 }
