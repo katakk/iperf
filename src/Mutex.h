0cc6dce65bdae217695516a051d0b76ed75eecc8
katakk <kata@katakuri.sakura.ne.jp>
2014-03-02 20:31:28 +0900

    Update Mutex.h

--- src/Mutex.h
+++ src/Mutex.h
@@ -9,54 +9,35 @@
 #include "headers.h"
 
 #if   defined( HAVE_POSIX_THREAD )
-    typedef pthread_mutex_t Mutex;
-#elif defined( HAVE_WIN32_THREAD )
-    typedef HANDLE Mutex;
-#else
-    typedef int Mutex;
-#endif
 
-/* ------------------------------------------------------------------- */
-    // initialize mutex
-#if   defined( HAVE_POSIX_THREAD )
-    #define Mutex_Initialize( MutexPtr ) pthread_mutex_init( MutexPtr, NULL )
-#elif defined( HAVE_WIN32_THREAD )
-    #define Mutex_Initialize( MutexPtr ) *MutexPtr = CreateMutex( NULL, false, NULL )
-#else
-    #define Mutex_Initialize( MutexPtr )
-#endif
+typedef pthread_mutex_t Mutex;
+#    define Mutex_Initialize( MutexPtr ) pthread_mutex_init( MutexPtr, NULL )
+#    define Mutex_Lock( MutexPtr ) pthread_mutex_lock( MutexPtr )
+#    define Mutex_Unlock( MutexPtr ) pthread_mutex_unlock( MutexPtr )
+#    define Mutex_Destroy( MutexPtr )  do {         \
+    int rc = pthread_mutex_destroy( MutexPtr ); \
+    if ( rc == EBUSY ) {                        \
+        Mutex_Unlock( MutexPtr );               \
+        pthread_mutex_destroy( MutexPtr );      \
+    }                                           \
+} while ( 0 )
 
-    // lock the mutex variable
-#if   defined( HAVE_POSIX_THREAD )
-    #define Mutex_Lock( MutexPtr ) pthread_mutex_lock( MutexPtr )
 #elif defined( HAVE_WIN32_THREAD )
-    #define Mutex_Lock( MutexPtr ) WaitForSingleObject( *MutexPtr, INFINITE )
-#else
-    #define Mutex_Lock( MutexPtr )
-#endif
 
-    // unlock the mutex variable
-#if   defined( HAVE_POSIX_THREAD )
-    #define Mutex_Unlock( MutexPtr ) pthread_mutex_unlock( MutexPtr )
-#elif defined( HAVE_WIN32_THREAD )
-    #define Mutex_Unlock( MutexPtr ) ReleaseMutex( *MutexPtr )
-#else
-    #define Mutex_Unlock( MutexPtr )
-#endif
+typedef HANDLE Mutex;
+#    define Mutex_Initialize( MutexPtr ) *MutexPtr = CreateMutex( NULL, false, NULL )
+#    define Mutex_Lock( MutexPtr ) WaitForSingleObject( *MutexPtr, INFINITE )
+#    define Mutex_Unlock( MutexPtr ) ReleaseMutex( *MutexPtr )
+#    define Mutex_Destroy( MutexPtr ) CloseHandle( *MutexPtr )
 
-    // destroy, making sure mutex is unlocked
-#if   defined( HAVE_POSIX_THREAD )
-    #define Mutex_Destroy( MutexPtr )  do {         \
-        int rc = pthread_mutex_destroy( MutexPtr ); \
-        if ( rc == EBUSY ) {                        \
-            Mutex_Unlock( MutexPtr );               \
-            pthread_mutex_destroy( MutexPtr );      \
-        }                                           \
-    } while ( 0 )
-#elif defined( HAVE_WIN32_THREAD )
-    #define Mutex_Destroy( MutexPtr ) CloseHandle( *MutexPtr )
 #else
-    #define Mutex_Destroy( MutexPtr )
+
+typedef int Mutex;
+#    define Mutex_Initialize( MutexPtr )
+#    define Mutex_Lock( MutexPtr )
+#    define Mutex_Unlock( MutexPtr )
+#    define Mutex_Destroy( MutexPtr )
+
 #endif
 
 #endif // MUTEX_H
