9983e95b9d076180ca7c974f8ed9145dad590cf3
kata <kata@katakuri.sakura.ne.jp>
2014-03-15 08:32:10 +0900

    new file:   t/test.bat 	new file:   t/test0bps.bat

---
+++ t/test.bat
@@ -0,0 +1,69 @@
+@echo off
+echo ------------------------------------------------------------
+echo Client connecting to 192.168.0.195, TCP port 5001
+echo TCP window size: 1.00 MByte
+echo ------------------------------------------------------------
+echo [1908] local 192.168.0.1 port 1234 connected with 192.168.0.195 port 5001
+echo [ ID] Interval       Transfer     Bandwidth
+echo [1908]  0.0- 1.0 sec  6.13 MBytes  51.4 Mbits/sec
+echo [1908]  1.0- 2.0 sec  5.38 MBytes  45.1 Mbits/sec
+echo [1908]  2.0- 3.0 sec  5.50 MBytes  46.1 Mbits/sec
+echo [1908]  3.0- 4.0 sec  5.88 MBytes  49.3 Mbits/sec
+echo [1908]  4.0- 5.0 sec  6.13 MBytes  51.4 Mbits/sec
+echo [1908]  5.0- 6.0 sec  5.88 MBytes  49.3 Mbits/sec
+echo [1908]  6.0- 7.0 sec  6.25 MBytes  52.4 Mbits/sec
+echo [1908]  7.0- 8.0 sec  5.38 MBytes  45.1 Mbits/sec
+echo [1908]  8.0- 9.0 sec  4.63 MBytes  38.8 Mbits/sec
+echo [1908]  9.0-10.0 sec  5.13 MBytes  43.0 Mbits/sec
+echo [1908] 10.0-11.0 sec  5.00 MBytes  41.9 Mbits/sec
+echo [1908] 11.0-12.0 sec  4.75 MBytes  39.8 Mbits/sec
+echo [1908] 12.0-13.0 sec  4.88 MBytes  40.9 Mbits/sec
+echo [1908] 13.0-14.0 sec  5.13 MBytes  43.0 Mbits/sec
+echo [1908] 14.0-15.0 sec  4.88 MBytes  40.9 Mbits/sec
+echo [1908] 15.0-16.0 sec  5.00 MBytes  41.9 Mbits/sec
+echo [1908] 16.0-17.0 sec  5.25 MBytes  44.0 Mbits/sec
+echo [1908] 17.0-18.0 sec  4.88 MBytes  40.9 Mbits/sec
+echo [1908] 18.0-19.0 sec  4.88 MBytes  40.9 Mbits/sec
+echo [1908] 19.0-20.0 sec  5.00 MBytes  41.9 Mbits/sec
+echo [1908] 20.0-21.0 sec  4.88 MBytes  40.9 Mbits/sec
+echo [1908] 21.0-22.0 sec  4.88 MBytes  40.9 Mbits/sec
+echo [1908] 22.0-23.0 sec  5.00 MBytes  41.9 Mbits/sec
+echo [1908] 23.0-24.0 sec  6.25 MBytes  52.4 Mbits/sec
+echo [1908] 24.0-25.0 sec  5.13 MBytes  43.0 Mbits/sec
+echo [1908] 25.0-26.0 sec  5.63 MBytes  47.2 Mbits/sec
+echo [1908] 26.0-27.0 sec  5.38 MBytes  45.1 Mbits/sec
+echo [1908] 27.0-28.0 sec  4.88 MBytes  40.9 Mbits/sec
+echo [1908] 28.0-29.0 sec  7.00 MBytes  58.7 Mbits/sec
+echo [1908] 29.0-30.0 sec  6.25 MBytes  52.4 Mbits/sec
+echo [1908] 30.0-31.0 sec  9.25 MBytes  77.6 Mbits/sec
+echo [1908] 31.0-32.0 sec  7.75 MBytes  65.0 Mbits/sec
+echo [1908] 32.0-33.0 sec  5.75 MBytes  48.2 Mbits/sec
+echo [1908] 33.0-34.0 sec  6.75 MBytes  56.6 Mbits/sec
+echo [1908] 34.0-35.0 sec  8.63 MBytes  72.4 Mbits/sec
+echo [1908] 35.0-36.0 sec  6.75 MBytes  56.6 Mbits/sec
+echo [1908] 36.0-37.0 sec  7.88 MBytes  66.1 Mbits/sec
+echo [1908] 37.0-38.0 sec  5.50 MBytes  46.1 Mbits/sec
+echo [1908] 38.0-39.0 sec  6.13 MBytes  51.4 Mbits/sec
+echo [1908] 39.0-40.0 sec  5.63 MBytes  47.2 Mbits/sec
+echo [1908] 40.0-41.0 sec  6.38 MBytes  53.5 Mbits/sec
+echo [1908] 41.0-42.0 sec  6.38 MBytes  53.5 Mbits/sec
+echo [1908] 42.0-43.0 sec  5.38 MBytes  45.1 Mbits/sec
+echo [1908] 43.0-44.0 sec  5.75 MBytes  48.2 Mbits/sec
+echo [1908] 44.0-45.0 sec  5.75 MBytes  48.2 Mbits/sec
+echo [1908] 45.0-46.0 sec  4.88 MBytes  40.9 Mbits/sec
+echo [1908] 46.0-47.0 sec  4.88 MBytes  40.9 Mbits/sec
+echo [1908] 47.0-48.0 sec  5.25 MBytes  44.0 Mbits/sec
+echo [1908] 48.0-49.0 sec  5.00 MBytes  41.9 Mbits/sec
+echo [1908] 49.0-50.0 sec  4.75 MBytes  39.8 Mbits/sec
+echo [1908] 50.0-51.0 sec  5.13 MBytes  43.0 Mbits/sec
+echo [1908] 51.0-52.0 sec  5.50 MBytes  46.1 Mbits/sec
+echo [1908] 52.0-53.0 sec  5.00 MBytes  41.9 Mbits/sec
+echo [1908] 53.0-54.0 sec  5.50 MBytes  46.1 Mbits/sec
+echo [1908] 54.0-55.0 sec  7.13 MBytes  59.8 Mbits/sec
+echo [1908] 55.0-56.0 sec  9.63 MBytes  80.7 Mbits/sec
+echo [1908] 56.0-57.0 sec  6.38 MBytes  53.5 Mbits/sec
+echo [1908] 57.0-58.0 sec  10.3 MBytes  86.0 Mbits/sec
+echo [1908] 58.0-59.0 sec  7.63 MBytes  64.0 Mbits/sec
+echo [1908] 59.0-60.0 sec  10.1 MBytes  84.9 Mbits/sec
+echo [1908]  0.0-60.5 sec   361 MBytes  50.1 Mbits/sec
+echo 
---
+++ t/test0bps.bat
@@ -0,0 +1,68 @@
+@echo off
+echo ------------------------------------------------------------
+echo Client connecting to 192.168.0.195, TCP port 5001
+echo TCP window size: 1.00 MByte
+echo ------------------------------------------------------------
+echo [1908] local 192.168.0.1 port 1234 connected with 192.168.0.195 port 5001
+echo [ ID] Interval       Transfer     Bandwidth
+echo [1908]  0.0- 1.0 sec  6.13 MBytes  0 Mbits/sec
+echo [1908]  1.0- 2.0 sec  5.38 MBytes  0 Mbits/sec
+echo [1908]  2.0- 3.0 sec  5.50 MBytes  0 Mbits/sec
+echo [1908]  3.0- 4.0 sec  5.88 MBytes  0 Mbits/sec
+echo [1908]  4.0- 5.0 sec  6.13 MBytes  0 Mbits/sec
+echo [1908]  5.0- 6.0 sec  5.88 MBytes  0 Mbits/sec
+echo [1908]  6.0- 7.0 sec  6.25 MBytes  0 Mbits/sec
+echo [1908]  7.0- 8.0 sec  5.38 MBytes  0 Mbits/sec
+echo [1908]  8.0- 9.0 sec  4.63 MBytes  0 Mbits/sec
+echo [1908]  9.0-10.0 sec  5.13 MBytes  0 Mbits/sec
+echo [1908] 10.0-11.0 sec  5.00 MBytes  0 Mbits/sec
+echo [1908] 11.0-12.0 sec  4.75 MBytes  0 Mbits/sec
+echo [1908] 12.0-13.0 sec  4.88 MBytes  0 Mbits/sec
+echo [1908] 13.0-14.0 sec  5.13 MBytes  0 Mbits/sec
+echo [1908] 14.0-15.0 sec  4.88 MBytes  0 Mbits/sec
+echo [1908] 15.0-16.0 sec  5.00 MBytes  0 Mbits/sec
+echo [1908] 16.0-17.0 sec  5.25 MBytes  0 Mbits/sec
+echo [1908] 17.0-18.0 sec  4.88 MBytes  0 Mbits/sec
+echo [1908] 18.0-19.0 sec  4.88 MBytes  0 Mbits/sec
+echo [1908] 19.0-20.0 sec  5.00 MBytes  0 Mbits/sec
+echo [1908] 20.0-21.0 sec  4.88 MBytes  0 Mbits/sec
+echo [1908] 21.0-22.0 sec  4.88 MBytes  0 Mbits/sec
+echo [1908] 22.0-23.0 sec  5.00 MBytes  0 Mbits/sec
+echo [1908] 23.0-24.0 sec  6.25 MBytes  0 Mbits/sec
+echo [1908] 24.0-25.0 sec  5.13 MBytes  0 Mbits/sec
+echo [1908] 25.0-26.0 sec  5.63 MBytes  0 Mbits/sec
+echo [1908] 26.0-27.0 sec  5.38 MBytes  0 Mbits/sec
+echo [1908] 27.0-28.0 sec  4.88 MBytes  0 Mbits/sec
+echo [1908] 28.0-29.0 sec  7.00 MBytes  0 Mbits/sec
+echo [1908] 29.0-30.0 sec  6.25 MBytes  0 Mbits/sec
+echo [1908] 30.0-31.0 sec  9.25 MBytes  0 Mbits/sec
+echo [1908] 31.0-32.0 sec  7.75 MBytes  0 Mbits/sec
+echo [1908] 32.0-33.0 sec  5.75 MBytes  0 Mbits/sec
+echo [1908] 33.0-34.0 sec  6.75 MBytes  0 Mbits/sec
+echo [1908] 34.0-35.0 sec  8.63 MBytes  0 Mbits/sec
+echo [1908] 35.0-36.0 sec  6.75 MBytes  0 Mbits/sec
+echo [1908] 36.0-37.0 sec  7.88 MBytes  0 Mbits/sec
+echo [1908] 37.0-38.0 sec  5.50 MBytes  0 Mbits/sec
+echo [1908] 38.0-39.0 sec  6.13 MBytes  0 Mbits/sec
+echo [1908] 39.0-40.0 sec  5.63 MBytes  0 Mbits/sec
+echo [1908] 40.0-41.0 sec  6.38 MBytes  0 Mbits/sec
+echo [1908] 41.0-42.0 sec  6.38 MBytes  0 Mbits/sec
+echo [1908] 42.0-43.0 sec  5.38 MBytes  0 Mbits/sec
+echo [1908] 43.0-44.0 sec  5.75 MBytes  0 Mbits/sec
+echo [1908] 44.0-45.0 sec  5.75 MBytes  0 Mbits/sec
+echo [1908] 45.0-46.0 sec  4.88 MBytes  0 Mbits/sec
+echo [1908] 46.0-47.0 sec  4.88 MBytes  0 Mbits/sec
+echo [1908] 47.0-48.0 sec  5.25 MBytes  0 Mbits/sec
+echo [1908] 48.0-49.0 sec  5.00 MBytes  0 Mbits/sec
+echo [1908] 49.0-50.0 sec  4.75 MBytes  0 Mbits/sec
+echo [1908] 50.0-51.0 sec  5.13 MBytes  0 Mbits/sec
+echo [1908] 51.0-52.0 sec  5.50 MBytes  0 Mbits/sec
+echo [1908] 52.0-53.0 sec  5.00 MBytes  0 Mbits/sec
+echo [1908] 53.0-54.0 sec  5.50 MBytes  0 Mbits/sec
+echo [1908] 54.0-55.0 sec  7.13 MBytes  0 Mbits/sec
+echo [1908] 55.0-56.0 sec  9.63 MBytes  0 Mbits/sec
+echo [1908] 56.0-57.0 sec  6.38 MBytes  0 Mbits/sec
+echo [1908] 57.0-58.0 sec  10.3 MBytes  0 Mbits/sec
+echo [1908] 58.0-59.0 sec  7.63 MBytes  0 Mbits/sec
+echo [1908] 59.0-60.0 sec  10.1 MBytes  0 Mbits/sec
+echo [1908]  0.0-60.5 sec   361 MBytes  0 Mbits/sec
